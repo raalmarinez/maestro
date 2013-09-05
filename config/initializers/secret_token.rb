e41a0cf334fb4aad0c479b71fcf8f240bf2a55bc
roderick <raalmarinez@hotmail.com>
2013-09-04 18:00:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maestro::Application.config.secret_token = '67ad0c3299ffbd01ebe2d01b78fd36b66088ce33d1cfc1d9302ebc301c1f62e3a3b6b846f524098bee4400d1d4e562ef7ff33afb76c4a2ec763c5698146c90ad'
